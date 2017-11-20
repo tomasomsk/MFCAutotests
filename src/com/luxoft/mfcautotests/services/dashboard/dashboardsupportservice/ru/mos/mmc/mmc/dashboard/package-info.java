ac983b8e8b72624cbf0a78c57549e4c2a60a7b8d
stomaschuk <t-o-m-a-s@mail.ru>
2017-11-20 14:37:31 +0600

    750_24 (in progress), 750_27 (complete), 750_39 (complete)

--- src/com/luxoft/mfcautotests/services/dashboard/dashboardsupportservice/ru/mos/mmc/mmc/dashboard/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://mmc.mos.ru/mmc/dashboard", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package ru.mos.mmc.mmc.dashboard;
